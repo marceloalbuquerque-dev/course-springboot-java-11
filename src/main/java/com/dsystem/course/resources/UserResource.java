b73464f9d84a416b9264225aaaa9c533ae4698d7
Marcelo Albuquerque <marceloradeveloper@gmail.com>
2020-11-20 21:34:59 -0300

    User entity and resource

---
+++ src/main/java/com/dsystem/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.dsystem.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dsystem.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Flavia", "flavia@gmail.com", "85965563225", "12345");
+		return ResponseEntity.ok().body(u);		
+	}
+}
